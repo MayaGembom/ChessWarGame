898333fcef965495fc4e8b85c11fb5af58b606b3
mayagembom <mayagembom3007@gmail.com>
2022-04-23 20:01:03 +0300

    Initial Commit

---
+++ ChessWarGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChessWarGame
+//
+//  Created by Maya on 23/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
